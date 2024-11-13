0b651841e5d25cab7db61a02d732fd515a28815a
Ankit Verma <ankitverma3103@gmail.com>
2024-11-13 16:39:02 +0530

    coffee demo

---
+++ js/app.js
@@ -0,0 +1,66 @@
+const container = document.querySelector('.container')
+
+const coffees = [
+  {
+    name: 'Espresso',
+    image: 'images/coffee1.jpg',
+  },
+  {
+    name: 'Americano',
+    image: 'images/coffee2.jpg',
+  },
+  {
+    name: 'Latte',
+    image: 'images/coffee3.jpg',
+  },
+  {
+    name: 'Cappuccino',
+    image: 'images/coffee4.jpg',
+  },
+  {
+    name: 'Macchiato',
+    image: 'images/coffee5.jpg',
+  },
+  {
+    name: 'Mocha',
+    image: 'images/coffee6.jpg',
+  },
+  {
+    name: 'Flat White',
+    image: 'images/coffee7.jpg',
+  },
+  {
+    name: 'Affogato',
+    image: 'images/coffee8.jpg',
+  },
+  {
+    name: 'Irish Coffee',
+    image: 'images/coffee9.jpg',
+  },
+]
+
+const showCoffees = () => {
+  let output = ''
+  coffees.forEach(
+    ({ name, image }) =>
+      (output += `
+              <div class="card">
+                <img class="card--avatar" src=${image} />
+                <h1 class="card--title">${name}</h1>
+                <a class="card--link" href="#">Taste</a>
+              </div>
+              `),
+  )
+  container.innerHTML = output
+}
+
+document.addEventListener('DOMContentLoaded', showCoffees)
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function () {
+    navigator.serviceWorker
+      .register('/serviceWorker.js')
+      .then((res) => console.log('service worker registered'))
+      .catch((err) => console.log('service worker not registered', err))
+  })
+}
